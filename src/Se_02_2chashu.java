ebf9a6cd579381da730003a9378667ae33089a52
nianhua <nianhua120qq@gmail.com>
2017-02-05 11:37:08 +0800

    实现了二叉树的排序

--- src/Se_02_2chashu.java
+++ src/Se_02_2chashu.java
@@ -1,19 +1,38 @@
-import javax.swing.tree.TreeNode;
+
+import java.util.Date;
 import java.util.Iterator;
 
 /**
  * Created by user on 2017/2/4.
  */
 public class Se_02_2chashu {
-    public static void main(String[] arg){
-        TreeNode treeNode;
-        //treeNode = new TreeNode(7);
+
+    public static void main(String[] arg) {
+        TreeNode treeNode = new TreeNode(7);
+        TreeNode treeNode1 = new TreeNode(5);
+        TreeNode treeNode2 = new TreeNode(10);
+        TreeNode treeNode3 = new TreeNode(3);
+        TreeNode treeNode4 = new TreeNode(6);
+        TreeNode treeNode5 = new TreeNode(9);
+        TreeNode treeNode6 = new TreeNode(20);
+        TreeNode treeNode7 = new TreeNode(1);
+        TreeNode treeNode8 = new TreeNode(4);
+        treeNode.left = treeNode1;
+        treeNode.right = treeNode2;
+        treeNode1.left = treeNode3;
+        treeNode1.right = treeNode4;
+        treeNode3.left = treeNode7;
+        treeNode3.right = treeNode8;
+        treeNode2.left = treeNode5;
+        treeNode2.right = treeNode6;
+        itera(treeNode);
         System.out.print("Hello ");
     }
-    void itera(TreeNode root){
-        if (root==null)return;
-        System.out.println(root);
+
+    static void itera(TreeNode root) {
+        if (root == null) return;
         itera(root.left);
+        System.out.println(root.value);
         itera(root.right);
     }
 }
---
+++ src/TreeNode.java
@@ -0,0 +1,14 @@
+/**
+ * Created by user on 2017/2/5.
+ */
+public class TreeNode {
+
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    public TreeNode(int val) {
+        this.value = val;
+    }
+
+}
